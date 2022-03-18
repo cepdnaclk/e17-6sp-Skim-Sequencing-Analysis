7b149cb44fbc1c3bb56a91e6ed8cec41df273157
DrMadhushan <dev.drmad@gmail.com>
2022-03-18 20:09:36 +0530

    implement read and write json methods

--- src/src/main/java/com/skimsequence/skimsequence/services/FileService.java
+++ src/src/main/java/com/skimsequence/skimsequence/services/FileService.java
@@ -1,13 +1,15 @@
 package com.skimsequence.skimsequence.services;
 
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+import com.skimsequence.skimsequence.NGSApplication;
 import javafx.stage.FileChooser;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileService {
 
@@ -30,13 +32,63 @@ public class FileService {
         return absPath;
     }
 
-    //To read a fq/fa file
-    public void readFile () {
-
+    public static void printJson(JsonObject json) {
+        json.forEach((key, value) -> System.out.println(key + " : " + value));
     }
 
-    public static void readJSON() {
+    public static void printJson(JsonArray json) {
+        json.forEach(item -> System.out.println(item));
+    }
 
+    public static JsonObject readJson(String path) {
+        /*
+         * Returns a json object in the file at path
+         */
+        URL url = NGSApplication.class.getResource(path);
+        JsonObject json = new JsonObject();
+        try {
+            // create a reader
+            Reader reader = Files.newBufferedReader(Paths.get(url.getPath()));
+
+            // create parser
+            JsonObject preferencesJson = (JsonObject) Jsoner.deserialize(reader);
+
+            // assign to return
+            json = preferencesJson;
+
+            //close reader
+            reader.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return json;
+    }
+
+    public static boolean writeJson(String key, String value, String path) {
+        /*
+         * Writes the key value pair to the Json file in the given path
+         */
+        URL url = NGSApplication.class.getResource(path);
+        try {
+            // create a writer
+            JsonObject json = readJson(path);
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get(url.getPath()));
+
+            // update particular key value pair
+            json.put(key, value);
+
+            // write to json
+            Jsoner.serialize(json, writer);
+            System.out.println("Written to json file " + path);
+
+            //close the writer
+            writer.close();
+            return true;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return false;
+        }
     }
 
     //To write to a file
